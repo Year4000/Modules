44d694b0e63fddced6c8214f6c8803e9acf7c666
Joshua Rodriguez <ewized@gmail.com>
2014-07-28 08:38:54 -0700

    Fix the WorldBackEvent

--- bukkit/src/main/java/net/year4000/hub/WorldBackEvent.java
+++ bukkit/src/main/java/net/year4000/hub/WorldBackEvent.java
@@ -3,7 +3,6 @@ package net.year4000.hub;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import org.bukkit.block.Block;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -12,9 +11,12 @@ import org.bukkit.event.HandlerList;
 @EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
 public class WorldBackEvent extends Event {
-    @Getter
     private static final HandlerList handlers = new HandlerList();
     private Block block;
+
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
     public static HandlerList getHandlerList() {
         return handlers;
