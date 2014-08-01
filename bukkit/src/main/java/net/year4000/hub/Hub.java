c5df8c0c4f48285c3c5d1b332bff8311cfe6a334
Joshua Rodriguez <ewized@gmail.com>
2014-08-01 10:58:04 -0700

    Protect spawn is now a constant

--- bukkit/src/main/java/net/year4000/hub/Hub.java
+++ bukkit/src/main/java/net/year4000/hub/Hub.java
@@ -14,6 +14,7 @@ import org.bukkit.*;
 )
 @ModuleListeners({HubListener.class, WorldListener.class, WorldBack.class})
 public class Hub extends BukkitModule {
+    public static final int SPAWN_PROTECTION = 20;
     WorldBack worldBack;
     public static final GameMode GAME_MODE = GameMode.ADVENTURE;
     
--- bukkit/src/main/java/net/year4000/hub/WorldListener.java
+++ bukkit/src/main/java/net/year4000/hub/WorldListener.java
@@ -71,7 +71,7 @@ public class WorldListener implements Listener {
         Location spawn = event.getClickedBlock().getLocation().getWorld().getSpawnLocation();
         Location pos = event.getClickedBlock().getLocation();
 
-        if (Math.sqrt(pos.distanceSquared(spawn)) < 20 && HubListener.mode(event.getPlayer())) {
+        if (Math.sqrt(pos.distanceSquared(spawn)) < Hub.SPAWN_PROTECTION && HubListener.mode(event.getPlayer())) {
             event.setCancelled(true);
         }
     }
