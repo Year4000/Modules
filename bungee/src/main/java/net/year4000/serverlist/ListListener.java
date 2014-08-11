b9072e8ea815eeb3559dc42de017cb4ea178a6f7
Joshua Rodriguez <ewized@gmail.com>
2014-08-10 20:15:14 -0700

    Motd properly show trunctated message (fixes #8)

--- bungee/src/main/java/net/year4000/serverlist/ListListener.java
+++ bungee/src/main/java/net/year4000/serverlist/ListListener.java
@@ -17,6 +17,7 @@ import net.md_5.bungee.event.EventPriority;
 import net.year4000.ducktape.bungee.DuckTape;
 import net.year4000.serverlist.messages.Message;
 import net.year4000.serverlist.messages.MessageFactory;
+import net.year4000.utilities.bungee.MessageUtil;
 
 import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
@@ -24,62 +25,71 @@ import java.util.concurrent.TimeUnit;
 import static net.year4000.utilities.bungee.MessageUtil.replaceColors;
 
 public class ListListener implements Listener {
-    private static final int MOTD_MAX = 45;
+    private static final int MOTD_MAX = 55; // 50 + 5 for indicator size
     private final LoadingCache<PingServer, ServerPing> ping = CacheBuilder.newBuilder()
         .maximumSize(1000)
         .expireAfterWrite(new Settings().getCache(), TimeUnit.SECONDS)
         .build(new CacheLoader<PingServer, ServerPing>() {
-               @Override
-               public ServerPing load(PingServer pingServer) throws Exception {
-                   ProxyServer proxy = ProxyServer.getInstance();
-                   Settings config = new Settings();
-                   ServerPing.PlayerInfo[] players;
-                   String motd = replaceColors(config.getPrefix());
-                   InetSocketAddress ip = pingServer.getConnection().getAddress();
-                   Message locale = new Message(pingServer.getLocale(ip));
-                   String message = MessageFactory.get().getMessage();
+            @Override
+            public ServerPing load(PingServer pingServer) throws Exception {
+               ProxyServer proxy = ProxyServer.getInstance();
+               Settings config = new Settings();
+               ServerPing.PlayerInfo[] players;
+               String motd = replaceColors(config.getPrefix());
+               InetSocketAddress ip = pingServer.getConnection().getAddress();
+               Message locale = new Message(pingServer.getLocale(ip));
+               String message = MessageFactory.get().getMessage();
 
-                   // Load the random messages top layer.
-                   ServerList.debug("Server List Message: " + message);
-                   motd += locale.get(Ascii.truncate(message, MOTD_MAX - motd.length(), "&7..."));
+               // Load the random messages top layer.
+               ServerList.debug("Server List Message: " + message);
+               int rawPrefixSize = motd.length() - Math.abs(MessageUtil.stripColors(motd).length() - motd.length());
+               motd += Ascii.truncate(locale.get(message), MOTD_MAX - rawPrefixSize, "&7...");
 
-                   // Load the player bottom layer and player sample.
-                   if (pingServer.getPlayer(ip) != null) {
-                       // Bottom row is a player is found.
-                       String motdPlayer = locale.get(config.getPlayer());
-                       motd += Ascii.truncate(motdPlayer.equals("") ? "" : " \n" + motdPlayer.replaceAll("\\{player\\}", pingServer.getPlayer(ip)), MOTD_MAX, "&7...");
+               // Load the player bottom layer and player sample.
+               if (pingServer.getPlayer(ip) != null) {
+                   // Bottom row is a player is found.
+                   String motdPlayer = locale.get(config.getPlayer());
+                   motd += motdPlayer.equals("") ? "" : " \n" + trueLength(motdPlayer.replaceAll("\\{player\\}", pingServer.getPlayer(ip)));
 
-                       // Set the player's ping to the one in the config.
-                       players = new ServerPing.PlayerInfo[config.getPlayers().size()];
-                       for (int i = 0; i < players.length; i++) {
-                           String line = config.getPlayers().get(i);
-                           players[i] = new ServerPing.PlayerInfo(locale.get(line), "");
-                       }
-                   } else {
-                       // Bottom row if no player is found.
-                       motd += Ascii.truncate(config.getNoPlayer().equals("") ? "" : " \n" + locale.get(config.getNoPlayer()), MOTD_MAX, "&7...");
+                   // Set the player's ping to the one in the config.
+                   players = new ServerPing.PlayerInfo[config.getPlayers().size()];
+                   for (int i = 0; i < players.length; i++) {
+                       String line = config.getPlayers().get(i);
+                       players[i] = new ServerPing.PlayerInfo(locale.get(line), "");
+                   }
+               } else {
+                   // Bottom row if no player is found.
+                   motd += config.getNoPlayer().equals("") ? "" : " \n" + trueLength(locale.get(config.getNoPlayer()));
 
-                       // Set the player's ping for players on the server.
-                       players = pingServer.getResponse().getPlayers().getSample();
+                   // Set the player's ping for players on the server.
+                   players = pingServer.getResponse().getPlayers().getSample();
 
-                       if (players != null && players.length == 0) {
-                           int playerCount = proxy.getOnlineCount();
-                           players = new ServerPing.PlayerInfo[playerCount];
+                   if (players != null && players.length == 0) {
+                       int playerCount = proxy.getOnlineCount();
+                       players = new ServerPing.PlayerInfo[playerCount];
 
-                           for (int i = 0; i < playerCount; i++) {
-                               String line = proxy.getPlayers().toArray()[i].toString();
-                               players[i] = new ServerPing.PlayerInfo(line, "");
-                           }
+                       for (int i = 0; i < playerCount; i++) {
+                           String line = proxy.getPlayers().toArray()[i].toString();
+                           players[i] = new ServerPing.PlayerInfo(line, "");
                        }
                    }
-
-                   // Set the Ping Response
-                   pingServer.getResponse().getPlayers().setSample(players);
-                   pingServer.getResponse().setDescription(motd);
-
-                   return pingServer.getResponse();
                }
-           });
+
+               // Set the Ping Response
+               pingServer.getResponse().getPlayers().setSample(players);
+               pingServer.getResponse().setDescription(MessageUtil.replaceColors(motd));
+
+               return pingServer.getResponse();
+            }
+
+            /** Strip colors to find out proper char length */
+            public String trueLength(String message) {
+                int trunk = Math.abs(MessageUtil.stripColors(message).length() - message.length()) + MOTD_MAX;
+
+                return Ascii.truncate(message, trunk, "&7...");
+            }
+
+        });
 
     @EventHandler
     public void onPostLogin(final PostLoginEvent event) {
