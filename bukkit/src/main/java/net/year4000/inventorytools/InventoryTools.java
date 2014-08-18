4b530ecebfca6d4cf5f3c36ea362e092eda7f025
Joshua Rodriguez <ewized@gmail.com>
2014-08-18 09:47:57 -0700

    Started work on inventory tools (for #12)

---
+++ bukkit/src/main/java/net/year4000/inventorytools/InventoryTools.java
@@ -0,0 +1,13 @@
+package net.year4000.inventorytools;
+
+import net.year4000.ducktape.bukkit.module.BukkitModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "Inventory Tools",
+    version = "1.0",
+    description = "Tools for handling inventories",
+    authors = {"Year4000"}
+)
+public class InventoryTools extends BukkitModule {
+}
