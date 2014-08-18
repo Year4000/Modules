4e1b11285255c4657319db33b86d61c75c817ebb
Joshua Rodriguez <ewized@gmail.com>
2014-08-18 09:52:04 -0700

    Started work on player tools (for #13)

---
+++ bukkit/src/main/java/net/year4000/playertools/PlayerTools.java
@@ -0,0 +1,13 @@
+package net.year4000.playertools;
+
+import net.year4000.ducktape.bukkit.module.BukkitModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "Player Tools",
+    version = "1.0",
+    description = "Tools for handling players.",
+    authors = {"Year4000"}
+)
+public class PlayerTools extends BukkitModule {
+}
---
+++ bungee/src/main/java/net/year4000/playertools/PlayerTools.java
@@ -0,0 +1,13 @@
+package net.year4000.playertools;
+
+import net.year4000.ducktape.bungee.module.BungeeModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "Player Tools",
+    version = "1.0",
+    description = "Tools for handling players.",
+    authors = {"Year4000"}
+)
+public class PlayerTools extends BungeeModule {
+}
