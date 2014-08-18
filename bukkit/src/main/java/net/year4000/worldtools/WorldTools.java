1624a0d2259fb4d2d2bf56b5df97685bb84be018
Joshua Rodriguez <ewized@gmail.com>
2014-08-18 09:49:57 -0700

    Started work on world tools (for #11)

---
+++ bukkit/src/main/java/net/year4000/worldtools/WorldTools.java
@@ -0,0 +1,13 @@
+package net.year4000.worldtools;
+
+import net.year4000.ducktape.bukkit.module.BukkitModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "World Tools",
+    version = "1.0",
+    description = "Tools for changing things around the world.",
+    authors = {"Year4000"}
+)
+public class WorldTools extends BukkitModule {
+}
