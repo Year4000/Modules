ca948fe46a55f7083ca98ed5d3b20b1f18a0dcaf
Joshua Rodriguez <ewized@gmail.com>
2014-08-18 09:40:40 -0700

    Started on the chat module (for #5)

---
+++ bukkit/src/main/java/net/year4000/chat/Chat.java
@@ -0,0 +1,13 @@
+package net.year4000.chat;
+
+import net.year4000.ducktape.bukkit.module.BukkitModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "Chat",
+    version = "1.6",
+    description = "Chat formatting with features.",
+    authors = {"Year4000"}
+)
+public class Chat extends BukkitModule {
+}
