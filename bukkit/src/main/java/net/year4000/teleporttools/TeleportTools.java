394389379263479683c830bb97e1df24bf826860
Joshua Rodriguez <ewized@gmail.com>
2014-08-18 09:45:19 -0700

    Started work on teleport tools (for #15)

---
+++ bukkit/src/main/java/net/year4000/teleporttools/TeleportTools.java
@@ -0,0 +1,13 @@
+package net.year4000.teleporttools;
+
+import net.year4000.ducktape.bukkit.module.BukkitModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "Teleport Tools",
+    version = "1.0",
+    description = "Master the magic of warping around the netowork",
+    authors = {"Year4000"}
+)
+public class TeleportTools extends BukkitModule {
+}
