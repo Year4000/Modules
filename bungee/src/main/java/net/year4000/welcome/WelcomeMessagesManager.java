ec9aece6042c460cf3b4e17c96f1aba1a875675f
Joshua Rodriguez <ewized@gmail.com>
2014-11-11 18:08:18 -0800

    Update locale urls for bungee modules

--- bungee/src/main/java/net/year4000/welcome/WelcomeMessagesManager.java
+++ bungee/src/main/java/net/year4000/welcome/WelcomeMessagesManager.java
@@ -5,7 +5,7 @@ import net.year4000.utilities.locale.URLLocaleManager;
 
 public class WelcomeMessagesManager extends URLLocaleManager {
     private static QuickCache<WelcomeMessagesManager> inst = QuickCache.builder(WelcomeMessagesManager.class).build();
-    private static String url = "https://git.year4000.net/year4000/locales/raw/master/net/year4000/hub/locales/";
+    private static String url = "https://raw.githubusercontent.com/Year4000/Locales/master/net/year4000/hub/locales/";
 
     public WelcomeMessagesManager() {
         super(Welcome.getLog(), url, parseJson(url + LOCALES_JSON));
