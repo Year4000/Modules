2d7dfa84371ade78b141d3443d8e1c41a1b9c174
Joshua Rodriguez <ewized@gmail.com>
2014-12-15 10:33:39 -0800

    Place animation in cache and add config settings for it

--- bungee/src/main/java/net/year4000/serverlist/ListListener.java
+++ bungee/src/main/java/net/year4000/serverlist/ListListener.java
@@ -4,6 +4,7 @@ import com.google.common.base.Ascii;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
@@ -20,67 +21,97 @@ import net.year4000.serverlist.messages.MessageFactory;
 import net.year4000.utilities.bungee.MessageUtil;
 
 import java.net.InetSocketAddress;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static net.year4000.utilities.bungee.MessageUtil.replaceColors;
 
 public class ListListener implements Listener {
     private static final int MOTD_MAX = 55; // 50 + 5 for indicator size
-    private final LoadingCache<PingServer, ServerPing> ping = CacheBuilder.newBuilder()
+    private final LoadingCache<PingServer, AbstractMap.Entry<ServerPing, ProxyPingEvent.AnimatedPing>> ping = CacheBuilder.newBuilder()
         .maximumSize(1000)
         .expireAfterWrite(new Settings().getCache(), TimeUnit.SECONDS)
-        .build(new CacheLoader<PingServer, ServerPing>() {
+        .build(new CacheLoader<PingServer, AbstractMap.Entry<ServerPing, ProxyPingEvent.AnimatedPing>>() {
             @Override
-            public ServerPing load(PingServer pingServer) throws Exception {
-               ProxyServer proxy = ProxyServer.getInstance();
-               Settings config = new Settings();
-               ServerPing.PlayerInfo[] players;
-               String motd = replaceColors(config.getPrefix());
-               InetSocketAddress ip = pingServer.getConnection().getAddress();
-               Message locale = new Message(pingServer.getLocale(ip));
-               String message = MessageFactory.get().getMessage();
+            public AbstractMap.Entry<ServerPing, ProxyPingEvent.AnimatedPing> load(PingServer pingServer) throws Exception {
+                ProxyServer proxy = ProxyServer.getInstance();
+                Settings config = new Settings();
+                ServerPing.PlayerInfo[] players;
+                String motd = replaceColors(config.getPrefix());
+                InetSocketAddress ip = pingServer.getConnection().getAddress();
+                Message locale = new Message(pingServer.getLocale(ip));
+                String message = MessageFactory.get().getMessage();
 
-               // Load the random messages top layer.
-               ServerList.debug("Server List Message: " + message);
-               int rawPrefixSize = motd.length() - Math.abs(MessageUtil.stripColors(motd).length() - motd.length());
-               motd += Ascii.truncate(locale.get(message), MOTD_MAX - rawPrefixSize, "&7...");
+                // Load the random messages top layer.
+                ServerList.debug("Server List Message: " + message);
+                int rawPrefixSize = motd.length() - Math.abs(MessageUtil.stripColors(motd).length() - motd.length());
+                motd += Ascii.truncate(locale.get(message), MOTD_MAX - rawPrefixSize, "&7...");
 
-               // Load the player bottom layer and player sample.
-               if (pingServer.getPlayer(ip) != null) {
-                   // Bottom row is a player is found.
-                   String motdPlayer = locale.get(config.getPlayer());
-                   motd += motdPlayer.equals("") ? "" : " \n" + trueLength(motdPlayer.replaceAll("\\{player\\}", pingServer.getPlayer(ip)));
+                // Load the player bottom layer and player sample.
+                if (pingServer.getPlayer(ip) != null) {
+                    // Bottom row is a player is found.
+                    String motdPlayer = locale.get(config.getPlayer());
+                    motd += motdPlayer.equals("") ? "" : " \n" + trueLength(motdPlayer.replaceAll("\\{player\\}", pingServer.getPlayer(ip)));
 
-                   // Set the player's ping to the one in the config.
-                   players = new ServerPing.PlayerInfo[config.getPlayers().size()];
-                   for (int i = 0; i < players.length; i++) {
-                       String line = config.getPlayers().get(i);
-                       players[i] = new ServerPing.PlayerInfo(locale.get(line), "");
-                   }
-               } else {
-                   // Bottom row if no player is found.
-                   motd += config.getNoPlayer().equals("") ? "" : " \n" + trueLength(locale.get(config.getNoPlayer()));
+                    // Set the player's ping to the one in the config.
+                    players = new ServerPing.PlayerInfo[config.getPlayers().size()];
+                    for (int i = 0; i < players.length; i++) {
+                        String line = config.getPlayers().get(i);
+                        players[i] = new ServerPing.PlayerInfo(locale.get(line), "");
+                    }
+                }
+                else {
+                    // Bottom row if no player is found.
+                    motd += config.getNoPlayer().equals("") ? "" : " \n" + trueLength(locale.get(config.getNoPlayer()));
 
-                   // Set the player's ping for players on the server.
-                   players = pingServer.getResponse().getPlayers().getSample();
+                    // Set the player's ping for players on the server.
+                    players = pingServer.getResponse().getPlayers().getSample();
 
-                   if (players != null && players.length == 0) {
-                       int playerCount = proxy.getOnlineCount();
-                       players = new ServerPing.PlayerInfo[playerCount];
+                    if (players != null && players.length == 0) {
+                        int playerCount = proxy.getOnlineCount();
+                        players = new ServerPing.PlayerInfo[playerCount];
 
-                       for (int i = 0; i < playerCount; i++) {
-                           String line = proxy.getPlayers().toArray()[i].toString();
-                           players[i] = new ServerPing.PlayerInfo(line, "");
-                       }
-                   }
-               }
+                        for (int i = 0; i < playerCount; i++) {
+                            String line = proxy.getPlayers().toArray()[i].toString();
+                            players[i] = new ServerPing.PlayerInfo(line, "");
+                        }
+                    }
+                }
 
-               // Set the Ping Response
-               pingServer.getResponse().getPlayers().setSample(players);
-               pingServer.getResponse().setDescription(MessageUtil.replaceColors(motd));
+                // Set the Ping Response
+                pingServer.getResponse().getPlayers().setSample(players);
+                pingServer.getResponse().setDescription(MessageUtil.replaceColors(motd));
 
-               return pingServer.getResponse();
+                // Set the animated ping if enabled
+                ProxyPingEvent.AnimatedPing ping = null;
+
+                if (config.isAnimated()) {
+                    ping = new ProxyPingEvent.AnimatedPing();
+                    int count = pingServer.getResponse().getPlayers().getOnline();
+                    int randCount = (new Random().nextInt(10) + 1);
+                    ping.setDelay(config.getAnimatedDelay());
+                    char[] original = pingServer.getResponse().getDescription().toCharArray();
+                    String type = "";
+
+                    for (int i = 0, j = -1; i <= count + randCount || j <= original.length; i += randCount) {
+                        if (i == 0) {
+                            ping.getResponses().add(pingServer.getResponse());
+                        }
+                        else {
+                            int online = i > count ? count : i;
+                            for (int c = 0;  c < 3; c++) {
+                                if (++j < original.length) {
+                                    type += original[j] == ChatColor.COLOR_CHAR ? original[j] +""+ original[++j] : original[j];
+                                }
+                            }
+
+                            ping.getResponses().add(new ServerPing(pingServer.getResponse().getVersion(), new ServerPing.Players(pingServer.getResponse().getPlayers().getMax(), online, pingServer.getResponse().getPlayers().getSample()), type, pingServer.getResponse().getFaviconObject()));
+                        }
+                        randCount = (new Random().nextInt(10) + 1);
+                    }
+                }
+
+               return new AbstractMap.SimpleImmutableEntry<>(pingServer.getResponse(), ping);
             }
 
             /** Strip colors to find out proper char length */
@@ -89,7 +120,6 @@ public class ListListener implements Listener {
 
                 return Ascii.truncate(message, trunk, "&7...");
             }
-
         });
 
     @EventHandler
@@ -120,22 +150,10 @@ public class ListListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onServerPing(ProxyPingEvent event) throws Exception {
-        ServerPing server = ping.getUnchecked(new PingServer(event.getConnection(), event.getResponse()));
+        AbstractMap.Entry<ServerPing, ProxyPingEvent.AnimatedPing> server = ping.getUnchecked(new PingServer(event.getConnection(), event.getResponse()));
         if (server != null) {
-            event.setResponse(server);
-
-            // todo re-enable when lag is fixed
-            /*if (server.getPlayers().getOnline() > 5) {
-                ProxyPingEvent.AnimatedPing ping = new ProxyPingEvent.AnimatedPing();
-                ping.setDelay(120);
-
-                for (int i = 0; i < server.getPlayers().getOnline(); i += (new Random().nextInt(2) + 1)) {
-                    int count = i == 0 ? server.getPlayers().getOnline() : i > server.getPlayers().getOnline() ? server.getPlayers().getOnline() : i;
-                    ping.getResponses().add(new ServerPing(server.getVersion(), new ServerPing.Players(server.getPlayers().getMax(), count, server.getPlayers().getSample()), server.getDescription(), server.getFaviconObject()));
-                }
-
-                event.setAnimatedPing(ping);
-            }*/
+            event.setResponse(server.getKey());
+            event.setAnimatedPing(server.getValue());
         }
     }
 }
