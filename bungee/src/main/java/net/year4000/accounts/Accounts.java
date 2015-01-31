8a6e09e1516032683fe728f2337bf83ed7b243b4
Joshua Rodriguez <ewized@gmail.com>
2015-01-31 08:35:50 -0800

    Simple accoutmodule to replace AccountLogin

---
+++ bungee/src/main/java/net/year4000/accounts/Accounts.java
@@ -0,0 +1,126 @@
+package net.year4000.accounts;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import net.md_5.bungee.api.event.LoginEvent;
+import net.md_5.bungee.api.event.PreLoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.year4000.ducktape.bungee.module.BungeeModule;
+import net.year4000.ducktape.bungee.module.ModuleListeners;
+import net.year4000.ducktape.module.ModuleInfo;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.*;
+
+@ModuleInfo(
+    name = "Accounts",
+    version = "2.0",
+    description = "Workaround for accounts until proper system is in place",
+    authors = {"Year4000"}
+)
+@ModuleListeners({Accounts.LoginListener.class})
+public class Accounts extends BungeeModule {
+    private static final String MASTER_KEY = "97be6c1b-0d4a-4e01-9b13-8ff923a8a5cd";
+    private static final String BASE_URL = "https://api.year4000.net/";
+    private static final Gson gson = new Gson();
+
+    public static class LoginListener implements Listener {
+        @EventHandler
+        public void onLogin(PreLoginEvent event) {
+            String ip = BASE_URL + "accounts/" + event.getConnection().getName() + "?key=" + MASTER_KEY;
+            String connectingIp = event.getConnection().getAddress().getAddress().getHostAddress();
+
+            // Get player account
+            try {
+                URL url = new URL(ip);
+                JsonObject object = gson.fromJson(new InputStreamReader(url.openStream()), JsonObject.class);
+                ip = object.get("last_ip").getAsString();
+            }
+            // Account does not exist
+            catch (IOException ioe) {
+                Accounts.debug(ioe, false);
+            }
+
+            // Check session server for offline connection
+            try (Socket socket = new Socket("sessionserver.mojang.com", 443)) {
+                if (socket.isConnected()) {
+                    Accounts.debug("Session servers online!");
+                }
+            }
+            catch (Exception e) {
+                event.getConnection().setOnlineMode(!connectingIp.equals(ip));
+            }
+        }
+
+        @EventHandler
+        public void onLogin(LoginEvent event) {
+            // Send login to the API server
+            try {
+                // Will throw if account does not exist
+                new URL(BASE_URL + "accounts/" + event.getConnection().getUniqueId()).openStream();
+
+                // Account exists lets login
+                URL url = new URL(BASE_URL + "accounts/" + event.getConnection().getUniqueId() + "/login?key=" + MASTER_KEY);
+                HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json; charset=utf8");
+                connection.setRequestProperty("User-Agent", "Year4000 Account Login - BungeeCord");
+                connection.setDoOutput(true);
+
+                try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())) {
+                    // Create JSON POST
+                    JsonObject login = new JsonObject();
+                    login.addProperty("type", "minecraft");
+                    login.addProperty("ip", event.getConnection().getAddress().getAddress().getHostAddress());
+                    JsonObject meta = new JsonObject();
+                    meta.addProperty("username", event.getConnection().getName());
+                    login.add("meta", meta);
+
+                    gson.toJson(login, writer);
+                }
+
+                if (connection.getResponseCode() != 200) {
+                    throw new IOException(connection.getResponseMessage());
+                }            }
+            catch (IOException ioe) {
+                Accounts.log(ioe, true);
+
+                // Send new account to API
+                try {
+                    // Create accounts
+                    URL url = new URL(BASE_URL + "accounts?key=" + MASTER_KEY);
+                    HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setRequestProperty("Content-Type", "application/json; charset=utf8");
+                    connection.setRequestProperty("User-Agent", "Year4000 Account Creation - BungeeCord");
+                    connection.setDoOutput(true);
+
+                    try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())) {
+                        // Create JSON POST
+                        JsonObject create = new JsonObject();
+                        create.addProperty("last_ip", event.getConnection().getAddress().getAddress().getHostAddress());
+                        JsonObject minecraft = new JsonObject();
+                        minecraft.addProperty("username", event.getConnection().getName());
+                        minecraft.addProperty("uuid", event.getConnection().getUniqueId().toString());
+                        create.add("minecraft", minecraft);
+
+                        gson.toJson(create, writer);
+                    }
+
+                    if (connection.getResponseCode() != 200) {
+                        throw new Exception(connection.getResponseMessage());
+                    }
+                }
+                // When all fails deny login and show exception error
+                catch (Exception e) {
+                    event.setCancelReason(e.getMessage());
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+}
