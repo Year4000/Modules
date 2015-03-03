31630259988225a6aaaa058959821a473112dd87
Joshua Rodriguez <ewized@gmail.com>
2015-03-03 06:34:35 -0800

    Make vortex look cooler with effects

--- bukkit/src/main/java/net/year4000/vortex/TimeVortex.java
+++ bukkit/src/main/java/net/year4000/vortex/TimeVortex.java
@@ -1,12 +1,18 @@
 package net.year4000.vortex;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import net.minecraft.server.v1_7_R4.Packet;
 import net.year4000.ducktape.bukkit.module.BukkitModule;
 import net.year4000.ducktape.bukkit.module.ModuleListeners;
 import net.year4000.ducktape.bukkit.utils.SchedulerUtil;
 import net.year4000.ducktape.module.ModuleInfo;
+import net.year4000.utilities.bukkit.BadgeManager;
+import net.year4000.utilities.bukkit.MessageUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,7 +24,12 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+import org.spigotmc.ProtocolInjector;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static net.year4000.utilities.bukkit.MessageUtil.replaceColors;
@@ -31,7 +42,116 @@ import static net.year4000.utilities.bukkit.MessageUtil.replaceColors;
 )
 @ModuleListeners({TimeVortex.class})
 public class TimeVortex extends BukkitModule implements Listener {
+    private static Set<String> shimmer = ImmutableSet.of("3", "b", "8", "7", "2", "a", "4", "c", "5", "d", "6", "e", "1", "9");
+    private static final String NAME = "Year4000";
+    private static final String IP = "mc.year4000.net";
+    private static Iterable<String> forever = Iterables.cycle(shimmer);
+    private static Iterator<String> color;
+    private static final BadgeManager manager = new BadgeManager();
+    private static Scoreboard scoreboard;
     private static final String ewized = "c9c2b7fe-e2c1-4266-9556-aafccc0d1f13";
+    private static final double X = 13.5;
+    private static final double Y = 90.2;
+    private static final double Z = 7.5;
+    private static final int YAW = 90;
+    private static final int PITCH = 0;
+
+    @Override
+    public void enable() {
+        color = forever.iterator();
+        scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+        SchedulerUtil.repeatAsync(() -> {
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                setTabListHeadFoot(player, getTabHeader(), getTabFooter());
+            });
+        }, 1, TimeUnit.SECONDS);
+    }
+
+    /** Set the tablist header and footer */
+    public static void setTabListHeadFoot(Player player, String header, String footer) {
+        if (!isEight(player)) return;
+
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+
+        Packet headFoot = new ProtocolInjector.PacketPlayOutPlayerListHeaderFooter(
+            sanitize(header),
+            sanitize(footer)
+        );
+
+        craftPlayer.getHandle().playerConnection.sendPacket(headFoot);
+    }
+
+    public static boolean isEight(Player player) {
+        return ((CraftPlayer) player).getHandle().playerConnection.networkManager.getVersion() >= 47;
+    }
+
+    /** Santize the string to be used in packets */
+    public static String sanitize(String text) {
+        if (text == null || text.length() == 0) {
+            return "\"\"";
+        }
+
+        char c;
+        int i;
+        int len = text.length();
+        StringBuilder sb = new StringBuilder(len + 4);
+        String t;
+        sb.append('"');
+
+        for (i = 0; i < len; i += 1) {
+            c = text.charAt(i);
+
+            switch (c) {
+                case '\\':
+                case '"':
+                    sb.append('\\');
+                    sb.append(c);
+                    break;
+                case '/':
+                    sb.append('\\');
+                    sb.append(c);
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                default:
+                    if (c < ' ') {
+                        t = "000" + Integer.toHexString(c);
+                        sb.append("\\u" + t.substring(t.length() - 4));
+                    }
+                    else {
+                        sb.append(c);
+                    }
+            }
+        }
+        sb.append('"');
+        return sb.toString();
+    }
+
+    public static String getTabFooter() {
+        String ip = "&b" + IP.replaceAll("\\.", "&3.&b");
+
+        return MessageUtil.replaceColors(ip);
+    }
+
+    public static String getTabHeader() {
+        String b = "&" + color.next();
+        String name = b + "[&" + color.next() + NAME + b + "]";
+
+        return MessageUtil.replaceColors(name);
+    }
 
     /** Return true | false if the map is running. */
     private boolean isMapPlaying(World world) {
@@ -42,24 +162,32 @@ public class TimeVortex extends BukkitModule implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onMapPlaying(PlayerInitialSpawnEvent event) {
-        event.setSpawnLocation(new Location(event.getPlayer().getWorld(), -1.5, 89.5, 4.5));
+        event.setSpawnLocation(new Location(event.getPlayer().getWorld(), X, Y, Z, YAW, PITCH));
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onMapPlaying(PlayerJoinEvent event) {
         event.setJoinMessage("");
         Player player = event.getPlayer();
-        player.teleport(new Location(player.getWorld(), -1.5, 89.5, 4.5));
+        SchedulerUtil.runAsync(() -> {
+            Team team = scoreboard.getTeam(player.getName()) == null ? scoreboard.registerNewTeam(player.getName()) : scoreboard.getTeam(player.getName());
+            team.setPrefix(manager.getBadge(player) + replaceColors(" &b"));
+            team.setSuffix(replaceColors("&f"));
+            team.add(player.getName());
+            player.setScoreboard(scoreboard);
+        });
+        player.teleport(new Location(player.getWorld(), X, Y, Z, YAW, PITCH));
         player.setExp(1F);
         player.setTotalExperience(0);
         player.sendMessage("");
         player.sendMessage(replaceColors("&6You were lost in the time vortex."));
-        player.sendMessage(replaceColors("&6Luckily this &eTime Capsule &6has found you."));
+        player.sendMessage(replaceColors("&6Luckily a &eTime Capsule &6has found you."));
         player.sendMessage("");
-        SchedulerUtil.runSync(() -> {
+        SchedulerUtil.runAsync(() -> {
+            player.sendMessage("");
             player.sendMessage(replaceColors("&6Type &e/hub &6to return to your universe."));
             player.sendMessage("");
-        }, 30, TimeUnit.SECONDS);
+        }, 20, TimeUnit.SECONDS);
         Bukkit.getOnlinePlayers().forEach(entity -> {
             entity.hidePlayer(player);
             player.hidePlayer(entity);
@@ -88,7 +216,7 @@ public class TimeVortex extends BukkitModule implements Listener {
         boolean map = isMapPlaying(entity.getWorld());
 
         if (event.getCause() == EntityDamageEvent.DamageCause.VOID && map) {
-            entity.teleport(new Location(entity.getWorld(), -1.5, 89.5, 4.5));
+            entity.teleport(new Location(entity.getWorld(), X, Y, Z, YAW, PITCH));
         }
 
         event.setCancelled(map);
