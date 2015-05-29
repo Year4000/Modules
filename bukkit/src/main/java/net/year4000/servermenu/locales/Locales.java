2dd5966a1191efbd16fa5a2a955286b237f1f82e
Joshua Rodriguez <ewized@gmail.com>
2015-05-29 13:29:58 -0700

    Locales for the app

---
+++ bukkit/src/main/java/net/year4000/servermenu/locales/Locales.java
@@ -0,0 +1,29 @@
+package net.year4000.servermenu.locales;
+
+import org.bukkit.command.CommandSender;
+
+public enum Locales {
+    MENU_SERVERS,
+    MENU_PLAYERS,
+    MENU_CLICK,
+    MENU_OPEN,
+    MENU_CLOSE,
+
+    SERVER_ONLINE,
+    SERVER_OFFLINE,
+    SERVER_CLICK,
+    SERVER_CONNECT,
+    ;
+
+    /** Translate the Locales enum to locale in the players language */
+    public String translate(CommandSender player, String... args) {
+        String code = this.name().toLowerCase().replaceAll("__", "-").replaceAll("_", ".");
+        return new MessageFactory.Message(player).get(code, args);
+    }
+
+    /** Translate the Locales enum to locale in the players language */
+    public String translate(String locale, String... args) {
+        String code = this.name().toLowerCase().replaceAll("__", "-").replaceAll("_", ".");
+        return new MessageFactory.Message(locale).get(code, args);
+    }
+}
---
+++ bukkit/src/main/java/net/year4000/servermenu/locales/MessageFactory.java
@@ -0,0 +1,37 @@
+package net.year4000.servermenu.locales;
+
+import net.year4000.ducktape.bukkit.DuckTape;
+import net.year4000.servermenu.Settings;
+import net.year4000.utilities.bukkit.BukkitLocale;
+import net.year4000.utilities.cache.QuickCache;
+import net.year4000.utilities.locale.URLLocaleManager;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class MessageFactory extends URLLocaleManager {
+    private static QuickCache<MessageFactory> inst = QuickCache.builder(MessageFactory.class).build();
+    private static String url = Settings.get().getLocales();
+
+    public MessageFactory() {
+        super(DuckTape.get().getLog(), url, parseJson(url + LOCALES_JSON));
+    }
+
+    public static MessageFactory get() {
+        return inst.get();
+    }
+
+    public static class Message extends BukkitLocale {
+        /** Use a CommandSender to create a locale */
+        public Message(CommandSender sender) {
+            super(sender instanceof Player ? (Player) sender : null);
+            localeManager = MessageFactory.get();
+        }
+
+        /** Allow using raw locale codes */
+        public Message(String code) {
+            super(null);
+            locale = code;
+            localeManager = MessageFactory.get();
+        }
+    }
+}
