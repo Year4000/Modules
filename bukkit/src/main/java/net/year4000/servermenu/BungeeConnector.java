2ffaa97860550270de36ea338f81019ac9f789be
Joshua Rodriguez <ewized@gmail.com>
2015-05-29 13:32:00 -0700

    Use BungeeConnector instead of BungeeSender

---
+++ bukkit/src/main/java/net/year4000/servermenu/BungeeConnector.java
@@ -0,0 +1,34 @@
+package net.year4000.servermenu;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import net.year4000.utilities.bukkit.MessagingChannel;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+@AllArgsConstructor
+public class BungeeConnector implements Listener {
+    @Getter
+    @Setter
+    private static String currentServer = null;
+    private static MessagingChannel connector = MessagingChannel.get();
+    private String server;
+
+    /** Send the player to the given server */
+    public void send(Player player) {
+        String[] data = new String[]{"Connect", server};
+        connector.sendToPlayer(player, data);
+    }
+
+    /** When a player joins get the name of the current server */
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (currentServer != null) return;
+
+        String[] data = new String[]{"GetServer"};
+        connector.send(data, (server, error) -> setCurrentServer(server.readUTF()));
+    }
+}
