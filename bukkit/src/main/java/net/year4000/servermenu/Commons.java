822735f7575a32d489fcc10d2b298f65e8b921c5
Joshua Rodriguez <ewized@gmail.com>
2015-05-30 17:08:51 -0700

    Added new Commons to replace Common

--- bukkit/src/main/java/net/year4000/servermenu/Commons.java
+++ bukkit/src/main/java/net/year4000/servermenu/Commons.java
@@ -7,8 +7,11 @@ import net.minecraft.server.v1_7_R4.NBTTagList;
 import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.StringTokenizer;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class Common {
+public class Commons {
+    /** Add a glow to item */
     public static ItemStack addGlow(ItemStack item) {
         net.minecraft.server.v1_7_R4.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
         NBTTagCompound tag = null;
@@ -23,6 +26,7 @@ public final class Common {
         return CraftItemStack.asCraftMirror(nmsStack);
     }
 
+    /** Remove glow from item */
     public static ItemStack removeGlow(ItemStack item) {
         net.minecraft.server.v1_7_R4.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
         NBTTagCompound tag = null;
@@ -31,5 +35,32 @@ public final class Common {
         tag.set("ench", null);
         nmsStack.setTag(tag);
         return CraftItemStack.asCraftMirror(nmsStack);
+    }
+
+    /** Split the string and limit the chars in a line */
+    public static String[] splitIntoLine(String input, int maxCharInLine){
+        StringTokenizer tok = new StringTokenizer(input, " ");
+        StringBuilder output = new StringBuilder(input.length());
+        int lineLen = 0;
+        while (tok.hasMoreTokens()) {
+            String word = tok.nextToken();
+
+            while(word.length() > maxCharInLine){
+                output.append(word.substring(0, maxCharInLine-lineLen) + "\n");
+                word = word.substring(maxCharInLine-lineLen);
+                lineLen = 0;
+            }
+
+            if (lineLen + word.length() > maxCharInLine) {
+                output.append("\n");
+                lineLen = 0;
+            }
+            output.append(word + " ");
+
+            lineLen += word.length() + 1;
+        }
+        // output.split();
+        // return output.toString();
+        return output.toString().split("\n");
     }
 }
