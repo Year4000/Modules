090b23423ff2f6b7ef19e045f5b1553e9a8e9e2a
Joshua Rodriguez <ewized@gmail.com>
2015-07-06 15:21:36 -0700

    Skelton for Vote module

---
+++ bungee/vote/src/main/java/net/year4000/vote/Vote.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2015 Year4000. All Rights Reserved.
+ */
+
+package net.year4000.vote;
+
+import net.year4000.ducktape.bungee.module.BungeeModule;
+import net.year4000.ducktape.module.ModuleInfo;
+
+@ModuleInfo(
+    name = "Vote",
+    version = "1.0.0",
+    description = "Voting for in game effects",
+    authors = {"Year4000"}
+)
+public class Vote extends BungeeModule {
+
+}
